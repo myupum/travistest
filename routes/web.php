39b65e29a7b7b48773a0f8d5a68550d794062944
myupum <myu1107@gmail.com>
2016-09-30 12:00:18 +0900

    エンジニア定例　Laravel演習①

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+Route::get('/hello',  'HelloController@index');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/hello', function () {
+//    return view('hello.index');
+//});
